72d7801a6292e366b73f9cf22267029118a06b22
Angelo Morgado <amorgado@pentafinanciero.cl>
2020-11-25 12:17:28 -0300

    FIX: Se modifica script build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 echo '...cleaning distribution folder'
-rm -rf dist && mkdir dist && mkdir dist/layers && mkdir dist/tmp
-
-echo '...building production libraries node_modules'
-cd ./libs && rm -rf node_modules && npm install && tsc && cd ..
-
-echo '...packaging layer'
-cd ./src && claudia pack --force --output ../dist/tmp/layer.zip >> /dev/null && cd ..
-unzip dist/tmp/layer.zip 'node_modules/*' -d dist/layers/nodejs/ >> /dev/null && rm -rf dist/tmp
-cd ./dist/layers && zip -r9 ../layers.zip nodejs >> /dev/null && cd ../.. &&
+rm -rf dist && mkdir dist && mkdir dist/tmp
 
 echo '...building production source code node_modules'
 cd ./src && rm -rf node_modules && npm install >> /dev/null && cd ..
